6e7f958b08b5272885ff6f06f36fc4b52640f415
arif <muhdarifrawi@gmail.com>
2022-06-24 14:38:53 +0800

    Following changes were made: - Added alternative method to import images. - Added CSS styling.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"; // equivalent: const React = require('react')
 import logo from "./logo.svg";
+import "./style.css";
 
 // Create a React component
 function App(){
@@ -10,7 +11,8 @@ function App(){
       <h1>test</h1>
       <p>React don't allow multiple parents.</p>
       <p>It must be wrapped in a div or an inivisible div like React.Fragment</p>
-      <img src={logo}/>
+      <img id="logo" src={logo}/>
+      <img id="cake-pic" src={require("./images/cake.jpg")}/>
     </React.Fragment>
     
   )
